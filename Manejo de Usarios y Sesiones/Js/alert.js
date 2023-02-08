7f10c9eda874cfe9e2d92a648ec0862b4a08a0bc
AntonioChi89 <124626071+AntonioChi89@users.noreply.github.com>
2023-02-08 17:43:18 -0600

    Update alert.js

--- Manejo de Usarios y Sesiones/Js/alert.js
+++ Manejo de Usarios y Sesiones/Js/alert.js
@@ -1,4 +1,360 @@
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 
-function mialerta() {  
-    alert("!Por Favor Regístrese Para poder Realizar Esta Acción¡");
-} 
+body{
+    background-color: #fefffc;
+    color: #000000;
+    font-family: 'Poppins', sans-serif;
+   
+}
+:root{
+    --background: linear-gradient(135deg, #667eea7e 0%, #764ba298 100%);
+    --color-primary:#667eea; 
+    --color-secondary:#764ba2;
+    --boton-padding: 20px 40px; 
+}
+
+.container{
+    width: 90%;
+    margin: 0 auto;
+    max-width: 1200px;
+    overflow: hidden;
+    padding: 80px 0;
+}
+
+.subtitle{
+    color: var(--color-primary);
+    font-size: 2.5rem;
+    margin-bottom: 35px;
+}
+
+.hero{
+    height: 100vh;
+    background-image: var(--background), url(../imagenes/fondo.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center;
+    position: relative;
+}
+
+.hero .container{
+    padding: 0;
+}
+
+.nav{
+    display: flex;
+    justify-content: flex-end;
+    height: 40px;
+    align-items: center;
+    font-weight: 600;
+}
+
+.nav--footer{
+    font-weight: 300;
+    justify-content: flex-start;
+}
+
+.nav__items{
+    color: #0a0a0a;
+    text-decoration: none;
+    margin-right: 20px;
+    padding: 10px 15px;
+    font-weight: inherit;
+}
+
+.nav__items--cta{
+    border: 3px solid #fff;
+}
+
+.nav__items--footer{
+    padding: 10px;
+}
+
+/*aquí comienza la parte del hero */
+.hero__title{
+    font-size: 3.2rem;   
+}
+
+.hero__subtitle{
+    font-size: 2rem;
+    font-weight: 300;
+    margin: 15px 0;
+    font-family: 'Poppins', sans-serif;
+}
+
+.hero__cta{
+    display: inline-block;
+    background: #e6e2eb;
+    padding: var(--boton-padding);
+    color: #000;
+    text-decoration: none;
+    font-weight: bold;
+    border-radius: 30px;
+    font-family: 'Poppins', sans-serif;
+   
+}
+
+.hero__conatiner{
+    display: flex;
+    height: calc(100vh - 70px);
+    align-items: center;
+    color: #080808
+}
+
+.hero_wave{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100px;
+}
+
+/* about */
+
+.presentation{
+    padding-top: 0;
+    text-align: center;
+    
+}
+
+.presentation__picture{
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    margin-bottom: 10px;
+    object-fit: cover;
+    object-position: top;
+}
+
+.presentation__copy{
+    width: 80%;
+    margin: 0 auto;
+}
+
+.presentation__cta{
+    display: inline-block;
+    margin-top: 30px;
+    background: var(--color-primary);
+    padding: var(--boton-padding);
+    color: #fff;
+    text-decoration: none;
+    border-radius: 30px;
+    
+}
+
+.about{
+    min-height: 400px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 80px;
+    justify-items: center;
+    align-items: center;
+    
+}
+
+.about__img{
+    text-align: center;
+}
+
+.about__img--left{
+    text-align: left;
+}
+
+.about_picture{
+    max-width: 80%;
+}
+
+.about__paragraph{
+    margin-bottom: 23px;
+    line-height: 1.8;
+    font-weight:400
+}
+
+/* products */
+
+.products{
+    background: #28282b;
+}
+
+/* fooster*/
+
+.footer{
+    background: #3a59e4;
+}
+
+.nav__items{
+    color:#fefffc;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 22px;
+}
+.footer__grid{
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    gap: 20px;
+    align-items: center;
+}
+
+.footer__title{
+    font-weight: 500;
+    color: #fff;
+    font-size: 2rem;
+    margin-bottom: 30px;
+    text-align: center;
+}
+
+.footer__icons{
+    display: flex;
+    justify-content: space-evenly;
+   
+}
+
+.footer__conatiner-icons{
+    display: inline-block;
+    width: 70px;
+    height: 70px;
+    color: #000000;
+    text-align: center;
+    border: 2px solid #0f0f0f;
+    border-radius: 66%;
+}
+
+.footer__icon{
+    color: inherit;
+    font-size: 35px;
+    text-decoration: none;
+    line-height: 65px;
+    text-align: center;
+}
+
+.fab .footer__icon{
+    line-height: 63px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+
+}
+
+/*area de la ventana emergente*/
+
+.overlay{
+    background: rgba(0, 0, 0, .5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    visibility: hidden;
+}
+
+.popup.active{
+    visibility: visible;
+}
+
+
+.popup{
+    background: #fff;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, .4);
+    border-radius: 40px;
+    font-family: 'Poppins', sans-serif;
+    padding: 25px;
+    text-align: center;
+    width: 600px;
+    transition: .2 ease all;
+    transform: scale(0.7);
+    opacity: 0;
+    
+
+}
+
+.popup .btn-cerrar-popup{
+    font-size: 20px;
+    line-height: 17px;
+    display: block;
+    text-align: right;
+    color: #BBBBBB;
+    transition: .3s ease all;
+    color:#000;
+
+}
+
+.popup .btn-cerrar-popup:hover{
+    color: red;
+}
+
+.popup img{
+    height: 80px;
+    width: 80px;
+}
+.popup h3{
+    font-size: 36px;
+    font-weight: 500;
+    margin-bottom: 2px;
+}
+
+.popup h4{
+    font-size: 20px;
+    font-weight: 300;
+    margin-bottom: 13px;
+}
+
+.popup input[type="submit"]{
+    padding: 0 20px;
+    height: 40px;
+    line-height: 40px;
+    border: none;
+    color:#0c0c0c;
+    background: orange;
+    border-radius: 15px;
+    cursor: pointer;
+    transition: .3s ease all;
+    font-size: 18px;
+    font-weight: bold;
+
+}
+
+.popup input[type="submit"]:hover{
+    color: #fff;
+}
+
+
+/* ------------------------- */
+/* ANIMACIONES */
+/* ------------------------- */
+.popup.active {	transform: scale(1); opacity: 1; }
+.popup.active h3 { animation: entradaTitulo .8s ease .5s forwards; }
+.popup.active h4 { animation: entradaSubtitulo .8s ease .5s forwards; }
+.popup.active .contenedor-inputs { animation: entradaInputs 1s linear 1s forwards; }
+
+@keyframes entradaTitulo {
+	from {
+		opacity: 0;
+		transform: translateY(-25px);
+	}
+
+	to {
+		transform: translateY(0);
+		opacity: 1;
+	}
+}
+
+@keyframes entradaSubtitulo {
+	from {
+		opacity: 0;
+		transform: translateY(25px);
+	}
+
+	to {
+		transform: translateY(0);
+		opacity: 1;
+	}
+}
+
+@keyframes entradaInputs {
+	from { opacity: 0; }
+	to { opacity: 1; }
+}
